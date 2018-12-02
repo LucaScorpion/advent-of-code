7741de829a1028380a84abce281626e49f169395
Luca Scalzotto <luca@scorpiac.com>
2018-12-02 21:27:58 +0100

    Finish day 1

---
+++ day_1/README.md
@@ -0,0 +1,18 @@
+### Usage
+
+```bash
+./solution.sh < input.txt
+```
+
+### Output
+
+```
+Resulting frequency: 430
+Calibrated frequency: 462
+Calibration took 144 additional iterations.
+```
+
+### Possible Improvements
+
+The script first read the entire input into an array before it starts processing.
+This costs an extra iteration and restricts the use of infinite inputs.
--- day_1/solution.sh
+++ day_1/solution.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
 set -euo pipefail
 
-total=0
+input=()
+frequency=0
 
-# Read all lines from the input, adding them to the total.
+# Frequency calibration.
+declare -A frequencies
+frequencies['0']=1
+calibrated=false
+iterations=0
+
+function checkFrequencies
+{
+    # Add one to the frequency counter.
+    local count=${frequencies["$frequency"]-0}
+    let count+=1
+    frequencies["$frequency"]=$count
+
+    # Check if we are calibrated.
+    if [ $count = 2 ]
+    then
+        calibrated=true
+        echo "Calibrated frequency: $frequency"
+    fi
+}
+
+function processInput
+{
+    for change in ${input[*]}
+    do
+        let frequency+=$change
+        checkFrequencies
+
+        # If we are calibrated, stop processing.
+        if [ $calibrated = true ]
+        then
+            return
+        fi
+    done
+}
+
+# Read the complete input.
 while IFS='' read -r line || [[ -n "$line" ]]
 do
-    let total+=line
-done < 'input.txt'
+    input[${#input[*]}]=$line
+done
 
-echo $total
+# Process the input once, display the resulting frequency.
+processInput
+echo "Resulting frequency: $frequency"
+
+# Continue calibration.
+while [ $calibrated = false ]
+do
+    let iterations+=1
+    processInput
+done
+
+echo "Calibration took $iterations additional iterations."
