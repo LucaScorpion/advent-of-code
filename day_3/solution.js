33119ac9be98af00b492d7c257702ed4cf16954a
Luca Scalzotto <luca@scorpiac.com>
2018-12-03 22:49:47 +0100

    Day 3 part 2

--- day_3/solution.js
+++ day_3/solution.js
@@ -6,28 +6,44 @@ const reader = readline.createInterface({
     input: process.stdin
 });
 
-const claimRegex = /^#\d+ @ (\d+),(\d+): (\d+)x(\d+)$/;
+const claimRegex = /^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$/;
 
 let fabric = [];
+// A claim is valid if it is the only one.
+let claims = {};
 
 process.stdin.on('end', () => {
     let claimedPieces = fabric
-        .map(col => col ? col.reduce((acc, val) => acc + (val > 1 ? 1 : 0), 0) : 0)
+        .map(col => {
+            if (!col) {
+                return 0;
+            }
+            return col.reduce((acc, val) => acc + (val.length > 1 ? 1 : 0), 0);
+        })
         .reduce((acc, val) => acc + val, 0);
 
     console.log('Claimed pieces:', claimedPieces);
+
+    let validClaims = Object.values(claims)
+        .filter(c => c.valid)
+        .map(c => c.id);
+
+    console.log('Valid claim id:', validClaims.join(', '));
 });
 reader.on('line', processLine);
 
 function processLine(line) {
     const groups = claimRegex.exec(line);
     const claim = {
-        left: parseInt(groups[1], 10),
-        top: parseInt(groups[2], 10),
-        width: parseInt(groups[3], 10),
-        height: parseInt(groups[4], 10)
+        id: groups[1],
+        left: parseInt(groups[2], 10),
+        top: parseInt(groups[3], 10),
+        width: parseInt(groups[4], 10),
+        height: parseInt(groups[5], 10),
+        valid: true
     };
 
+    claims[claim.id] = claim;
     claimFabric(claim);
 }
 
@@ -35,16 +51,26 @@ function claimFabric(claim) {
     // CLAIM EVERY INCH!!!
     for (let x = claim.left; x < claim.left + claim.width; x++) {
         for (let y = claim.top; y < claim.top + claim.height; y++) {
-            claimInch(x, y);
+            claimInch(claim.id, x, y);
         }
     }
 }
 
-function claimInch(x, y) {
+function claimInch(id, x, y) {
     if (!fabric[x]) {
         fabric[x] = [];
     }
+    if (!fabric[x][y]) {
+        fabric[x][y] = [];
+    }
 
     // Apply the claim.
-    fabric[x][y] = (fabric[x][y] || 0) + 1;
+    if (!fabric[x][y].includes(id)) {
+        fabric[x][y].push(id);
+    }
+
+    // If there is more than 1 claim, invalidate them all.
+    if (fabric[x][y].length > 1) {
+        fabric[x][y].forEach(i => claims[i].valid = false);
+    }
 }
