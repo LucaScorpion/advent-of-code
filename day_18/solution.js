bd4ddf8eeaa9b0f8380ca541086e8151584c4ca6
Luca Scalzotto <luca@scorpiac.com>
2018-12-19 17:55:44 +0100

    Day 18 part 1

--- day_18/solution.js
+++ day_18/solution.js
@@ -6,7 +6,7 @@ const reader = readline.createInterface({
     input: process.stdin
 });
 
-const GROUND = '.';
+const OPEN = '.';
 const TREES = '|';
 const LUMBERYARD = '#';
 
@@ -37,5 +37,62 @@ function getResult() {
 }
 
 function step() {
+    let newState = [];
 
+    for (let y = 0; y < state.length; y++) {
+        newState[y] = [];
+
+        for (let x = 0; x < state[y].length; x++) {
+            let adj = getAdjacent(x, y);
+            let type = state[y][x];
+
+            switch (type) {
+                case OPEN:
+                    if (countAdjacent(x, y, TREES) >= 3) {
+                        type = TREES;
+                    }
+                    break;
+                case TREES:
+                    if (countAdjacent(x, y, LUMBERYARD) >= 3) {
+                        type = LUMBERYARD;
+                    }
+                    break;
+                case LUMBERYARD:
+                    if (countAdjacent(x, y, LUMBERYARD) === 0 || countAdjacent(x, y, TREES) === 0) {
+                        type = OPEN;
+                    }
+                    break;
+            }
+
+            newState[y][x] = type;
+        }
+    }
+
+    state = newState;
+}
+
+function countAdjacent(x, y, type) {
+    return getAdjacent(x, y).filter(t => t === type).length;
+}
+
+function getAdjacent(x, y) {
+    let adj = [];
+
+    for (let yy = y - 1; yy <= y + 1; yy++) {
+        for (let xx = x - 1; xx <= x + 1; xx++) {
+            // Check bounds.
+            if (yy < 0 || yy >= state.length || xx < 0 || xx >= state[yy].length) {
+                continue;
+            }
+
+            // Ignore the tile itself.
+            if (xx === x && yy === y) {
+                continue;
+            }
+
+            adj.push(state[yy][xx]);
+        }
+    }
+
+    return adj;
 }
