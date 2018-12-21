6eea43f3856543d05f8c227c02b629eb9b367874
Luca Scalzotto <luca@scorpiac.com>
2018-12-21 23:55:52 +0100

    Input parsing somewhat working

--- day_17/solution.js
+++ day_17/solution.js
@@ -9,51 +9,103 @@ const reader = readline.createInterface({
 const SAND = '.';
 const WATER = '~';
 const CLAY = '#';
+const SOURCE = '+';
 
-let yBounds = {
-    min: null,
-    max: null
+let bounds = {
+    x: null,
+    y: null
 };
-let clayBlocks = [];
 let grid = [];
 
 process.stdin.on('end', getResult);
 reader.on('line', processLine);
 
 function getResult() {
-    // Build the grid.
+    // Fill the grid.
+    for (let y = 0; y <= bounds.y; y++) {
+        if (!grid[y]) {
+            grid[y] = [];
+        }
 
+        let row = grid[y];
+        for (let x = 0; x <= bounds.x; x++) {
+            if (!row[x]) {
+                row[x] = SAND;
+            }
+        }
+    }
 
-    // Place the water, process until stable.
+    // Place the source.
+    grid[0][10] = SOURCE;
+
+    simulateWater(10, 0);
+
+    grid.forEach(row => console.log(row.join('')));
 
     // Count the blocks of water.
     let waterCount = grid.reduce((acc, row) => acc + row.filter(block => block === WATER).length, 0);
     console.log(`Blocks of water: ${waterCount}`);
 }
 
+function simulateWater(startX, startY) {
+    waterDown(startX, startY);
+}
+
+function waterDown(x, y) {
+    while (waterCanMoveTo(x, y + 1)) {
+        y++;
+        grid[y][x] = WATER;
+    }
+}
+
+function waterCanMoveTo(x, y) {
+    if (y < 0 || y > bounds.y) {
+        return false;
+    }
+
+    return grid[y][x] === SAND;
+}
+
 function processLine(line) {
     let parts = line.split(', ');
 
-    let staticPart = parts[0].substr(2);
+    let staticPart = parseInt(parts[0].substr(2), 10);
     let range = parts[1].substr(2);
     let rangeParts = range.split('..');
 
-    let minY = 0;
-    let maxY = 0;
-
-    // Calculate the grid bounds.
-    if (yBounds.min === null || minY < yBounds.min) {
-        yBounds.min = minY;
-    }
-    if (yBounds.max === null || maxY > yBounds.max) {
-        yBounds.max = maxY;
-    }
+    let clayFunc = line.substr(0, 1) === 'x' ? verticalClay : horizontalClay;
+    clayFunc(staticPart, parseInt(rangeParts[0], 10), parseInt(rangeParts[1], 10));
 }
 
-function horizontalClay() {
+function horizontalClay(y, minX, maxX) {
+    for (let x = minX; x <= maxX; x++) {
+        addClay(x, y);
+    }
 
+    calcGridBounds(maxX, y);
 }
 
-function verticalClay() {
+function verticalClay(x, minY, maxY) {
+    for (let y = minY; y <= maxY; y++) {
+        addClay(x, y);
+    }
 
+    calcGridBounds(x, maxY);
+}
+
+function addClay(x, y) {
+    if (!grid[y]) {
+        grid[y] = [];
+    }
+
+    grid[y][x] = CLAY;
+}
+
+function calcGridBounds(x, y) {
+    if (bounds.x === null || x > bounds.x) {
+        bounds.x = x;
+    }
+    if (bounds.y === null || y > bounds.y) {
+        bounds.y = y;
+    }
 }
