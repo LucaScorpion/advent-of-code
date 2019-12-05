e193922d8624af07f2bcc93141e026f7b89f4f94
Luca Scalzotto <luca@scorpiac.com>
2019-12-06 00:22:19 +0100

    Day 4 part 2

--- 2019/day_4/solution.ts
+++ 2019/day_4/solution.ts
@@ -4,14 +4,12 @@ const input = fs.readFileSync(0).toString().trim().split('-');
 const from = parseInt(input[0], 10);
 const to = parseInt(input[1], 10);
 
-function check(code: number): boolean {
-  const codeStr = code.toString();
+function checkV1(code: string): boolean {
   let dubs = false;
+  let prevDigit = code[0];
 
-  // Use a string here, we can simply compare ASCII codes to check which number is higher, no need to re-parse.
-  let prevDigit = '0';
-  for (let i = 0; i < codeStr.length; i++) {
-    const currentDigit = codeStr[i];
+  for (let i = 1; i < code.length; i++) {
+    const currentDigit = code[i];
 
     if (prevDigit === currentDigit) {
       dubs = true;
@@ -25,10 +23,46 @@ function check(code: number): boolean {
   return dubs;
 }
 
-let found = 0;
+function checkV2(code: string): boolean {
+  let dubs = false;
+  let digitCounter = 1;
+  let prevDigit = code[0];
+
+  for (let i = 1; i < code.length; i++) {
+    const currentDigit = code[i];
+
+    if (prevDigit === currentDigit) {
+      digitCounter++;
+    } else if (currentDigit < prevDigit) {
+      return false;
+    } else {
+      // currentDigit > prevDigit
+      if (digitCounter === 2) {
+        dubs = true;
+      }
+      digitCounter = 1;
+    }
+
+    prevDigit = currentDigit;
+  }
+
+  if (digitCounter === 2) {
+    dubs = true;
+  }
+
+  return dubs;
+}
+
+let found1 = 0;
+let found2 = 0;
 for (let code = from; code <= to; code++) {
-  if (check(code)) {
-    found++;
+  const codeStr = code.toString();
+  if (checkV1(codeStr)) {
+    found1++;
+  }
+  if (checkV2(codeStr)) {
+    found2++;
   }
 }
-console.log('Found', found, 'possible codes');
+console.log('Found', found1, 'possible codes with the first criteria');
+console.log('Found', found2, 'possible codes with the second criteria');
