cc5e256dd3081c7c39b15694f367496ccb3b3fa6
Luca Scalzotto <luca@scorpiac.com>
2019-12-06 13:32:05 +0100

    Day 6 part 2

--- 2019/day_6/solution.ts
+++ 2019/day_6/solution.ts
@@ -1,9 +1,15 @@
 import fs from 'fs';
 
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const center = 'COM';
+
+enum Location {
+  CENTER = 'COM',
+  YOU = 'YOU',
+  SANTA = 'SAN',
+}
 
 const orbits: { [around: string]: string[] } = {};
+const reverseOrbits: { [obj: string]: string } = {};
 
 input.forEach(orbit => {
   const [around, obj] = orbit.split(')');
@@ -11,6 +17,7 @@ input.forEach(orbit => {
     orbits[around] = [];
   }
   orbits[around].push(obj);
+  reverseOrbits[obj] = around;
 });
 
 function getOrbitCount(around: string, indirect: number = 0): number {
@@ -18,4 +25,28 @@ function getOrbitCount(around: string, indirect: number = 0): number {
   return indirect + objs.map(obj => getOrbitCount(obj, indirect + 1)).reduce((sum, cur) => sum + cur, 0);
 }
 
-console.log('Total number of orbits:', getOrbitCount(center));
+function getDistance(from: string | undefined, to: string, soFar: number = 0, visited: Set<string> = new Set<string>()): number | false {
+  if (!from || visited.has(from)) {
+    return false;
+  }
+
+  // Check if we arrived.
+  if (from === to) {
+    return soFar;
+  }
+
+  // Copy the visited set.
+  visited = new Set<string>(visited);
+  visited.add(from);
+
+  // Get the distances, inward and outward.
+  const inDist = getDistance(reverseOrbits[from], to, soFar + 1, visited);
+  const outDists = (orbits[from] || []).map(out => getDistance(out, to, soFar + 1, visited));
+
+  // Filter out dead ends, return the smallest distance.
+  const distances = [inDist, ...outDists].filter(d => d !== false);
+  return distances.length > 0 ? distances.reduce((min, cur) => cur < min ? cur : min) : false;
+}
+
+console.log('Total number of orbits:', getOrbitCount(Location.CENTER));
+console.log('Orbital transfers required:', getDistance(reverseOrbits[Location.YOU], reverseOrbits[Location.SANTA]));
