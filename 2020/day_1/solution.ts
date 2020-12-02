3d049494a283bfcf6e52e14deb953cf6a74d44fa
Luca Scalzotto <luca@scorpiac.com>
2020-12-02 19:24:34 +0100

    Day 1 part 2

--- 2020/day_1/solution.ts
+++ 2020/day_1/solution.ts
@@ -3,6 +3,8 @@ import fs from 'fs';
 const input = fs.readFileSync(0).toString().trim().split(/\r?\n/)
   .map(s => parseInt(s, 10));
 
+// Part 1
+
 outer:
   for (let i = 0; i < input.length - 1; i++) {
     for (let j = i + 1; j < input.length; j++) {
@@ -12,6 +14,24 @@ outer:
         console.log(iVal, '+', jVal, '= 2020');
         console.log(iVal, '*', jVal, '=', iVal * jVal);
         break outer;
+      }
+    }
+  }
+
+// Part 2
+
+outer:
+  for (let i = 0; i < input.length - 2; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      for (let k = j + 1; k < input.length; k++) {
+        const iVal = input[i];
+        const jVal = input[j];
+        const kVal = input[k];
+        if (iVal + jVal + kVal === 2020) {
+          console.log(iVal, '+', jVal, '+', kVal, '= 2020');
+          console.log(iVal, '*', jVal, '*', kVal, '=', iVal * jVal * kVal);
+          break outer;
+        }
       }
     }
   }
