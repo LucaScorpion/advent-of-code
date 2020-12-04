8487f3427c24d7ffe8a8469420d482706dec3ece
Luca Scalzotto <luca@scorpiac.com>
2020-12-04 20:17:27 +0100

    Day 3 part 2

--- 2020/day_3/solution.ts
+++ 2020/day_3/solution.ts
@@ -5,26 +5,49 @@ const input = fs.readFileSync(0).toString().trim().split(/\r?\n/)
 
 type Cell = '.' | '#';
 
+interface Slope {
+  right: number;
+  down: number;
+}
+
+const slopes: Slope[] = [
+  { right: 1, down: 1 },
+  { right: 3, down: 1 },
+  { right: 5, down: 1 },
+  { right: 7, down: 1 },
+  { right: 1, down: 2 },
+];
+
 function getSquare(rowNum: number, colNum: number): Cell {
   const row = input[rowNum];
   return row[colNum % row.length];
 }
 
-function getTrajectory(right: number, down: number): Cell[] {
+function getTrajectory(slope: Slope): Cell[] {
   const trajectory: Cell[] = [];
-  let x = right;
-  let y = down;
+  let x = slope.right;
+  let y = slope.down;
 
   while (y < input.length) {
     trajectory.push(getSquare(y, x));
-    x += right;
-    y += down;
+    x += slope.right;
+    y += slope.down;
   }
 
   return trajectory;
 }
 
-const traj = getTrajectory(3, 1);
-const treeCount = traj.filter(c => c === '#').length;
+function countTrees(slope: Slope): number {
+  const traj = getTrajectory(slope);
+  return traj.filter(c => c === '#').length;
+}
 
-console.log('Encountered', treeCount, 'trees');
+let treesMultiplied = 1;
+
+slopes.forEach(s => {
+  const treeCount = countTrees(s);
+  console.log(`Slope ${s.right},${s.down}, trees: ${treeCount}`);
+  treesMultiplied *= treeCount;
+});
+
+console.log('Trees multiplied:', treesMultiplied);
