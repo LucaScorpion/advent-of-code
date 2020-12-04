e39469238d16d916e783da10d681bc33f0ee482a
Luca Scalzotto <luca@scorpiac.com>
2020-12-04 21:13:09 +0100

    Day 4 part 2

--- 2020/day_4/solution.ts
+++ 2020/day_4/solution.ts
@@ -33,16 +33,53 @@ const mandatoryField = [
   // 'cid'
 ];
 
-function isValid(passport: Passport): boolean {
+const validEyeColors = [ 'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth' ];
+
+function isPassportValid(passport: Passport): boolean {
   let isValid = true;
   mandatoryField.forEach(field => {
     const value = passport[field];
     if (value == undefined) {
       isValid = false;
+      return;
     }
+
+    const numValue = parseInt(value, 10);
+    switch (field) {
+      case 'byr':
+        isValid = isValid && (numValue >= 1920 && numValue <= 2002);
+        break;
+      case 'iyr':
+        isValid = isValid && (numValue >= 2010 && numValue <= 2020);
+        break;
+      case 'eyr':
+        isValid = isValid && (numValue >= 2020 && numValue <= 2030);
+        break;
+      case 'hgt':
+        const heightNum = parseInt(value.substring(0, value.length - 2), 10);
+        const unit = value.substring(value.length - 2);
+        if (unit == 'cm') {
+          isValid = isValid && (heightNum >= 150 && heightNum <= 193);
+        } else if (unit == 'in') {
+          isValid = isValid && (heightNum >= 59 && heightNum <= 76);
+        } else {
+          isValid = false;
+        }
+        break;
+      case 'hcl':
+        isValid = isValid && /^#[a-f0-9]{6}$/.test(value);
+        break;
+      case 'ecl':
+        isValid = isValid && validEyeColors.includes(value);
+        break;
+      case 'pid':
+        isValid = isValid && /^[0-9]{9}$/.test(value);
+        break;
+    }
+
   });
   return isValid;
 }
 
-const validPassports = passports.filter(isValid).length;
+const validPassports = passports.filter(isPassportValid).length;
 console.log(validPassports, 'valid passports');
