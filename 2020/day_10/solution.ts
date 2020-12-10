0ef6c2aea273fd089de8f85972ffd0b18a0e0f17
Luca Scalzotto <luca@scorpiac.com>
2020-12-10 20:31:15 +0100

    Actual working arrangement count

--- 2020/day_10/solution.ts
+++ 2020/day_10/solution.ts
@@ -18,50 +18,13 @@ const d3Count = differences.filter((d) => d === 3).length;
 
 console.log(d1Count, '*', d3Count, '=', d1Count * d3Count);
 
-function countArrangements(arrangement: number[], upperI: number): number {
-  let total = 0;
-
-  for (let i = upperI; i > 0; i--) {
-    const removed = [
-      ...arrangement.slice(0, i),
-      ...arrangement.slice(i + 1, arrangement.length),
-    ];
-
-    const lowVal = removed[i - 1];
-    const highVal = removed[i];
-    if (highVal - lowVal <= 3) {
-      total += 1 + countArrangements(removed, i - 1);
-    }
-  }
-
-  return total;
+function countArrangements(): number {
+  const diffString = differences.join('');
+  const groups = diffString.split('3').filter(v => !!v);
+  const twos = Math.pow(2, groups.filter(g => g.length === 2).length);
+  const threes = Math.pow(4, groups.filter(g => g.length === 3).length);
+  const fours = Math.pow(7, groups.filter(g => g.length === 4).length);
+  return twos * threes * fours;
 }
 
-
-console.log('Valid arrangements:', countArrangements(joltages, joltages.length - 2) + 1);
-
-
-// NAIVE IMPLEMENTATION, DOES NOT REALLY WORK
-// Also add +1 to result.
-function countArrangementsRecursive(startIndex: number): number {
-  if (startIndex % 10 === 0) {
-    console.log(startIndex);
-  }
-
-  if (startIndex === joltages.length - 1) {
-    return 0;
-  }
-
-  const sourceJoltage = joltages[startIndex];
-  let paths = -1;
-
-  for (let i = startIndex + 1; i < startIndex + 4 && i < joltages.length; i++) {
-    const adapterJoltage = joltages[i];
-    if (adapterJoltage - sourceJoltage <= 3) {
-      paths += 1 + countArrangementsRecursive(i);
-    }
-  }
-
-  return paths;
-}
-
+console.log('Valid arrangements:', countArrangements());
