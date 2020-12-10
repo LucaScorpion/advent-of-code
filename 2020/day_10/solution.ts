33c065a6bff551522fbd1012eca582c1c012b6ca
Luca Scalzotto <luca@scorpiac.com>
2020-12-10 20:13:34 +0100

    Slightly better, still slow

--- 2020/day_10/solution.ts
+++ 2020/day_10/solution.ts
@@ -18,7 +18,36 @@ const d3Count = differences.filter((d) => d === 3).length;
 
 console.log(d1Count, '*', d3Count, '=', d1Count * d3Count);
 
-function countArrangements(startIndex: number): number {
+function countArrangements(arrangement: number[], upperI: number): number {
+  let total = 0;
+
+  for (let i = upperI; i > 0; i--) {
+    const removed = [
+      ...arrangement.slice(0, i),
+      ...arrangement.slice(i + 1, arrangement.length),
+    ];
+
+    const lowVal = removed[i - 1];
+    const highVal = removed[i];
+    if (highVal - lowVal <= 3) {
+      total += 1 + countArrangements(removed, i - 1);
+    }
+  }
+
+  return total;
+}
+
+
+console.log('Valid arrangements:', countArrangements(joltages, joltages.length - 2) + 1);
+
+
+// NAIVE IMPLEMENTATION, DOES NOT REALLY WORK
+// Also add +1 to result.
+function countArrangementsRecursive(startIndex: number): number {
+  if (startIndex % 10 === 0) {
+    console.log(startIndex);
+  }
+
   if (startIndex === joltages.length - 1) {
     return 0;
   }
@@ -29,11 +58,10 @@ function countArrangements(startIndex: number): number {
   for (let i = startIndex + 1; i < startIndex + 4 && i < joltages.length; i++) {
     const adapterJoltage = joltages[i];
     if (adapterJoltage - sourceJoltage <= 3) {
-      paths += 1 + countArrangements(i);
+      paths += 1 + countArrangementsRecursive(i);
     }
   }
 
   return paths;
 }
 
-console.log('Valid arrangements:', countArrangements(0) + 1);
