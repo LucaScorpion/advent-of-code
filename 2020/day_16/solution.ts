9756f18436d11244cb706957c10201849e4c051b
Luca Scalzotto <luca@scorpiac.com>
2020-12-16 22:06:58 +0100

    Day 16 part 1

---
+++ 2020/day_16/solution.ts
@@ -0,0 +1,68 @@
+import fs from 'fs';
+
+const input = fs.readFileSync(0).toString().trim().split(/\r?\n/);
+let lineNumber = 0;
+
+interface Range {
+  low: number;
+  high: number;
+}
+
+interface Field {
+  name: string;
+  ranges: Range[];
+}
+
+const fields: Field[] = [];
+while (input[lineNumber]) {
+  const [name, ranges] = input[lineNumber].split(': ');
+  const [r1, r2] = ranges.split(' or ');
+  const [r1Low, r1High] = r1.split('-');
+  const [r2Low, r2High] = r2.split('-');
+
+  fields.push({
+    name,
+    ranges: [
+      { low: parseInt(r1Low, 10), high: parseInt(r1High, 10) },
+      { low: parseInt(r2Low, 10), high: parseInt(r2High, 10) },
+    ],
+  });
+
+  lineNumber++;
+}
+
+lineNumber += 2;
+const ourTicket = input[lineNumber].split(',').map((v) => parseInt(v, 10));
+
+lineNumber += 3;
+const nearbyTickets: number[][] = [];
+while (lineNumber < input.length) {
+  nearbyTickets.push(input[lineNumber].split(',').map((v) => parseInt(v, 10)));
+  lineNumber++;
+}
+
+function isInRange(value: number, range: Range): boolean {
+  return value >= range.low && value <= range.high;
+}
+
+function isInAnyRange(value: number, ranges: Range[]): boolean {
+  return ranges.filter((r) => isInRange(value, r)).length > 0;
+}
+
+let errorRate = 0;
+nearbyTickets.forEach((ticket) => {
+  ticket.forEach((val) => {
+    let isValidAnyField = false;
+    fields.forEach((field) => {
+      if (isInAnyRange(val, field.ranges)) {
+        isValidAnyField = true;
+      }
+    });
+
+    if (!isValidAnyField) {
+      errorRate += val;
+    }
+  });
+});
+
+console.log('Error rate:', errorRate);
