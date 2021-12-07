6d52d36465b2e21a8ccce924220af6a8455c8fc4
Luca Scalzotto <luca@scorpiac.com>
2021-12-07 21:40:03 +0100

    Day 7 part 2

--- 2021/day_7/solution.ts
+++ 2021/day_7/solution.ts
@@ -2,20 +2,38 @@ import fs from 'fs';
 
 const startPositions = fs.readFileSync(0).toString().trim().split(',').map(Number);
 
-function calcFuelReq(pos: number): number {
-  return startPositions.map((p) => Math.abs(pos - p)).reduce((acc, cur) => acc + cur, 0);
+function linearFuel(pos: number, target: number): number {
+  return Math.abs(target - pos);
 }
 
+function binomialFuel(pos: number, target: number): number {
+  const delta = Math.abs(target - pos);
+  return Math.floor((delta * (delta + 1)) / 2);
+}
+
+function totalFuelReq(target: number, fuelFunc: (pos: number, target: number) => number): number {
+  return startPositions.map((p) => fuelFunc(p, target)).reduce((acc, cur) => acc + cur, 0);
+}
+
+const minPosition = Math.min(...startPositions);
 const maxPosition = Math.max(...startPositions);
 
-let minFuelReq = Number.POSITIVE_INFINITY;
-let minFuelReqPos = 0;
-for (let i = 0; i <= maxPosition; i++) {
-  const fuelReq = calcFuelReq(i);
-  if (fuelReq < minFuelReq) {
-    minFuelReq = fuelReq;
-    minFuelReqPos = i;
+function minFuelReq(fuelFunc: (pos: number, target: number) => number): [number, number] {
+  let fuel = Number.POSITIVE_INFINITY;
+  let pos = 0;
+
+  for (let i = minPosition; i <= maxPosition; i++) {
+    const fuelReq = totalFuelReq(i, fuelFunc);
+    if (fuelReq < fuel) {
+      fuel = fuelReq;
+      pos = i;
+    }
   }
+
+  return [pos, fuel];
 }
 
-console.log(`Fuel requirement to align at position ${minFuelReqPos}: ${minFuelReq}`);
+const [linearPos, linearReq] = minFuelReq(linearFuel);
+console.log(`Linear fuel requirement to align at position ${linearPos}: ${linearReq}`);
+const [binomialPos, binomialReq] = minFuelReq(binomialFuel);
+console.log(`Binomial fuel requirement to align at position ${binomialPos}: ${binomialReq}`);
