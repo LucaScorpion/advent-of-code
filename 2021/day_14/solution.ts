88dc9b98b8923edd757c674d91a3ca7558d62d49
Luca Scalzotto <luca@scorpiac.com>
2021-12-15 14:11:45 +0100

    Day 14 part 2

--- 2021/day_14/solution.ts
+++ 2021/day_14/solution.ts
@@ -1,40 +1,56 @@
 import fs from 'fs';
 
 const [polymerTemplate, pairLines] = fs.readFileSync(0).toString().trim().split('\n\n');
-const STEPS = 10;
+const STEPS_1 = 10;
+const STEPS_2 = 40;
 
 const pairInsertions: Record<string, string> = Object.fromEntries(pairLines.split('\n').map((line) => line.split(' -> ')));
+let pairs: Record<string, number> = {};
 
-function processPolymer(p: string[]): string[] {
-  const result: string[] = [];
+for (let i = 0; i < polymerTemplate.length - 1; i++) {
+  const pairString = `${polymerTemplate[i]}${polymerTemplate[i + 1]}`;
+  pairs[pairString] = (pairs[pairString] || 0) + 1;
+}
 
-  for (let i = 0; i < p.length - 1; i++) {
-    result.push(p[i]);
+function processPolymer(old: Record<string, number>): Record<string, number> {
+  const result: Record<string, number> = {};
 
-    const insert = pairInsertions[`${p[i]}${p[i + 1]}`];
-    if (insert) {
-      result.push(insert);
+  Object.entries(old).forEach(([pair, count]) => {
+    let newPairLeft = pair;
+
+    if (pairInsertions[pair]) {
+      newPairLeft = `${pair[0]}${pairInsertions[pair]}`;
+
+      const newPairRight = `${pairInsertions[pair]}${pair[1]}`;
+      result[newPairRight] = (result[newPairRight] || 0) + count;
     }
-  }
-  result.push(p[p.length - 1]);
+
+    result[newPairLeft] = (result[newPairLeft] || 0) + count;
+  });
 
   return result;
 }
 
-let polymer = polymerTemplate.split('');
-for (let step = 0; step < STEPS; step++) {
-  polymer = processPolymer(polymer);
+function printResult(steps: number): void {
+  const elementCounts: Record<string, number> = {};
+  Object.entries(pairs).forEach(([pair, count]) => {
+    const elem = pair[0];
+    elementCounts[elem] = (elementCounts[elem] || 0) + count;
+  });
+  const lastElem = polymerTemplate[polymerTemplate.length - 1];
+  elementCounts[lastElem] = (elementCounts[lastElem] || 0) + 1;
+
+  const mostCommon = Math.max(...Object.values(elementCounts));
+  const leastCommon = Math.min(...Object.values(elementCounts));
+
+  console.log(`Most common - least common after ${steps} steps = ${mostCommon - leastCommon}`);
 }
 
-const elementCounts: Record<string, number> = {};
-polymer.forEach((elem) => {
-  if (!elementCounts[elem]) {
-    elementCounts[elem] = 0;
+for (let step = 0; step < STEPS_2; step++) {
+  pairs = processPolymer(pairs);
+
+  if (step === STEPS_1 - 1) {
+    printResult(STEPS_1);
   }
-  elementCounts[elem]++;
-});
-
-const mostCommon = Math.max(...Object.values(elementCounts));
-const leastCommon = Math.min(...Object.values(elementCounts));
-
-console.log(`Most common - least common = ${mostCommon - leastCommon}`);
+}
+printResult(STEPS_2);
