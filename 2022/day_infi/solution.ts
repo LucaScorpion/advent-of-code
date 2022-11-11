b7d69e9caa883bd9c2d5dfcc4a7f6b361a060b33
Luca Scalzotto <luca@scorpiac.com>
2022-11-11 14:38:10 +0100

    Infi part 2

--- 2022/day_infi/solution.ts
+++ 2022/day_infi/solution.ts
@@ -46,21 +46,63 @@ function manhattanDistance(vec: Vector2): number {
   return Math.abs(vec.x) + Math.abs(vec.y);
 }
 
-// Part 1
+function vecString(vec: Vector2): string {
+  return `${vec.x};${vec.y}`;
+}
 
 let pos: Vector2 = { x: 0, y: 0 };
 let rot = 0;
+const grid = {
+  x: [0, 0],
+  y: [0, 0],
+  steps: new Set<string>(),
+};
 
 instructions.forEach((i) => {
   if (i.type === 'draai') {
     rot += i.amount;
-  } else {
-    const rotVec = rotatedForwardVector(rot);
+    return;
+  }
+
+  const rotVec = rotatedForwardVector(rot);
+  if (i.type === 'spring') {
     pos = {
       x: pos.x + rotVec.x * i.amount,
       y: pos.y + rotVec.y * i.amount,
     };
+    setStep();
+  } else {
+    for (let d = 0; d < i.amount; d++) {
+      pos = {
+        x: pos.x + rotVec.x,
+        y: pos.y + rotVec.y,
+      };
+      setStep();
+    }
   }
 });
 
-console.log(manhattanDistance(pos));
+function setStep(): void {
+  if (pos.x < grid.x[0]) {
+    grid.x[0] = pos.x;
+  }
+  if (pos.x > grid.x[1]) {
+    grid.x[1] = pos.x;
+  }
+  if (pos.y < grid.y[0]) {
+    grid.y[0] = pos.y;
+  }
+  if (pos.y > grid.y[1]) {
+    grid.y[1] = pos.y;
+  }
+  grid.steps.add(vecString(pos));
+}
+
+console.log(`Manhattan distance moved: ${manhattanDistance(pos)}`);
+console.log('Path:\n');
+for (let y = grid.y[1]; y >= grid.y[0]; y--) {
+  for (let x = grid.x[0]; x <= grid.x[1]; x++) {
+    process.stdout.write(grid.steps.has(vecString({ x, y })) ? '#' : ' ');
+  }
+  process.stdout.write('\n');
+}
