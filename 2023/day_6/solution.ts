714f2f5ccf94225a2b72cb8eb6477ac4e6d3ad4d
Luca Scalzotto <luca@scorpiac.com>
2023-12-07 17:26:11 +0100

    Day 6 part 2

--- 2023/day_6/solution.ts
+++ 2023/day_6/solution.ts
@@ -9,7 +9,7 @@ interface Race {
   recordDistance: number;
 }
 
-const races: Race[] = timeCols.map((c, i) => ({
+const shortRaces: Race[] = timeCols.map((c, i) => ({
   duration: parseInt(c),
   recordDistance: parseInt(distCols[i]),
 }));
@@ -19,14 +19,22 @@ function calculateDistance(raceDuration: number, holdTime: number) {
   return moveTime * holdTime; // holdTime == speed
 }
 
-const winOptions = races.map((r) => {
+function calculateWinOptions(race: Race) {
   let winCount = 0;
-  for (let holdTime = 0; holdTime <= r.duration; holdTime++) {
-    if (calculateDistance(r.duration, holdTime) > r.recordDistance) {
+  for (let holdTime = 0; holdTime <= race.duration; holdTime++) {
+    if (calculateDistance(race.duration, holdTime) > race.recordDistance) {
       winCount++;
     }
   }
   return winCount;
-});
+}
 
-console.log(winOptions.reduce((acc, cur) => acc * cur, 1));
+const shortWinOptions = shortRaces.map(calculateWinOptions).reduce((acc, cur) => acc * cur, 1);
+console.log(`Short races win options: ${shortWinOptions}`);
+
+const longRace: Race = {
+  duration: parseInt(lines[0].substring(5).replaceAll(' ', '')),
+  recordDistance: parseInt(lines[1].substring(9).replaceAll(' ', '')),
+}
+const longWinOptions = calculateWinOptions(longRace);
+console.log(`Long race win options: ${longWinOptions}`);
