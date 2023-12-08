fc22f0dd7a7467ac780e07a0bf8a7ae68c87c64f
Luca Scalzotto <luca@scorpiac.com>
2023-12-08 20:14:50 +0100

    Day 8 part 2

--- 2023/day_8/solution.ts
+++ 2023/day_8/solution.ts
@@ -15,14 +15,43 @@ const nodes: Node[] = lines.slice(2).map((l) => ({
   right: l.substring(12, 15),
 }));
 
-let currentNode = nodes.find((n) => n.self === 'AAA')!;
-let stepCount = 0;
-while (currentNode.self !== 'ZZZ') {
-  const stepI = stepCount % steps.length;
-  const step = steps[stepI];
-  const nodeStep = step === 'L' ? currentNode.left : currentNode.right;
-  currentNode = nodes.find((n) => n.self === nodeStep)!;
-  stepCount++;
+
+function calculateSteps(startNode: string, endNodes: string[]) {
+  let currentNode = nodes.find((n) => n.self === startNode)!;
+  let stepCount = 0;
+  while (!endNodes.includes(currentNode.self)) {
+    const stepI = stepCount % steps.length;
+    const step = steps[stepI];
+    const nodeStep = step === 'L' ? currentNode.left : currentNode.right;
+    currentNode = nodes.find((n) => n.self === nodeStep)!;
+    stepCount++;
+  }
+  return stepCount;
 }
 
-console.log(`Steps taken: ${stepCount}`);
+function calculateStepsAll(startNodes: string[], endNodes: string[]) {
+  const allStepCounts = startNodes.map((n) => calculateSteps(n, endNodes));
+  return leastCommonMultipleArr(allStepCounts);
+}
+
+function greatestCommonDivisor(a: number, b: number): number {
+  return b === 0 ? a : greatestCommonDivisor(b, a % b);
+}
+
+function leastCommonMultiple(a: number, b: number) {
+  return (a * b) / greatestCommonDivisor(a, b);
+}
+
+function leastCommonMultipleArr(n: number[]) {
+  return n.reduce((acc, cur) => leastCommonMultiple(acc, cur), Math.min(...n));
+}
+
+console.log(`Steps taken: ${calculateSteps(
+  nodes.find((n) => n.self === 'AAA')!.self,
+  nodes.filter((n) => n.self === 'ZZZ').map((n) => n.self),
+)}`);
+
+console.log(`Ghost steps taken: ${calculateStepsAll(
+  nodes.filter((n) => n.self.endsWith('A')).map((n) => n.self),
+  nodes.filter((n) => n.self.endsWith('Z')).map((n) => n.self),
+)}`);
