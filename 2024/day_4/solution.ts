80f4e5f53b7db09f26fe8dcb256fee0563b9042a
Luca Scalzotto <luca@scalzotto.nl>
2024-12-05 15:10:08 +0100

    Day 4

--- 2024/day_4/solution.ts
+++ 2024/day_4/solution.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 const lines = fs.readFileSync(0).toString().trim().split('\n');
 
-const word = 'XMAS';
+const xmas = 'XMAS';
 
 function get(x: number, y: number): string | null {
   if (y < 0 || y >= lines.length || x < 0 || x >= lines[y].length) {
@@ -32,9 +32,9 @@ function countXmas(): number {
 function isXmas(originX: number, originY: number, [dX, dY]: [number, number], wordIndex = 0): boolean {
   const x = originX + (dX * wordIndex);
   const y = originY + (dY * wordIndex);
-  const match = get(x, y) === word[wordIndex];
+  const match = get(x, y) === xmas[wordIndex];
 
-  if (wordIndex === word.length - 1) {
+  if (wordIndex === xmas.length - 1) {
     return match;
   }
 
@@ -42,3 +42,33 @@ function isXmas(originX: number, originY: number, [dX, dY]: [number, number], wo
 }
 
 console.log(`Total XMAS: ${countXmas()}`);
+
+function countXmas2(): number {
+  let total = 0;
+
+  for (let y = 0; y < lines.length; y++) {
+    for (let x = 0; x < lines[y].length; x++) {
+      if (isXmas2(x, y)) {
+        total++;
+      }
+    }
+  }
+
+  return total;
+}
+
+function isXmas2(x: number, y: number): boolean {
+  if (get(x, y) !== 'A') {
+    return false;
+  }
+
+  const topLeftBottomRight = `${get(x - 1, y - 1)}${get(x + 1, y + 1)}`
+  const bottomLeftTopRight = `${get(x - 1, y + 1)}${get(x + 1, y - 1)}`
+
+  return (
+    (topLeftBottomRight === 'MS' || topLeftBottomRight === 'SM') &&
+    (bottomLeftTopRight === 'MS' || bottomLeftTopRight === 'SM')
+  );
+}
+
+console.log(`Total X-MAS: ${countXmas2()}`);
