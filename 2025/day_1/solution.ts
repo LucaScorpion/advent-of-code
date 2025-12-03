a69ea742a9150de06fef61f3a5139f3314812653
Luca Scalzotto <luca@scalzotto.nl>
2025-12-03 20:33:21 +0100

    Day 1

--- 2025/day_1/solution.ts
+++ 2025/day_1/solution.ts
@@ -11,17 +11,30 @@ class Dial {
   private static readonly MAX = 100;
 
   private value = 50;
+  private zeroes = 0;
 
   public rotate(rotation: Rotation) {
-    this.value += rotation.direction === 'R' ? rotation.amount : -rotation.amount;
-    while (this.value < 0) {
-      this.value += Dial.MAX;
+    const unit = rotation.direction === 'R' ? 1 : -1;
+    for (let i = 0; i < rotation.amount; i++) {
+      this.value += unit;
+
+      while (this.value < 0) {
+        this.value += Dial.MAX;
+      }
+      this.value %= Dial.MAX;
+
+      if (this.value === 0) {
+        this.zeroes++;
+      }
     }
-    this.value %= Dial.MAX;
   }
 
   public current(): number {
     return this.value;
+  }
+
+  public getZeroes(): number {
+    return this.zeroes;
   }
 }
 
@@ -29,13 +42,15 @@ const lines = fs.readFileSync(0).toString().trim().split('\n')
   .map((l): Rotation => ({direction: l[0] as Direction, amount: parseInt(l.substring(1), 10)}));
 
 const dial = new Dial();
-let zeroes = 0;
+let endZeroes = 0;
 
 lines.forEach((l) => {
   dial.rotate(l);
+
   if (dial.current() === 0) {
-    zeroes++;
+    endZeroes++;
   }
 });
 
-console.log(`Password: ${zeroes}`);
+console.log(`Password: ${endZeroes}`);
+console.log(`Password using 0x434C49434B: ${dial.getZeroes()}`);
